3399364e0c2cb5e905d8531b7c4f03c335599c43
shilin123-321 <shilinshibu@gmail.com>
2019-08-29 10:37:52 +0530

    qq

--- ping/ping1.c
+++ ping/ping1.c
@@ -15,32 +15,6 @@
 
 
 
-int pingloop=1; 
-
-
-// ping packet structure 
-struct ping_pkt 
-{ 
-	struct icmphdr hdr; 
-	char msg[PING_PKT_S-sizeof(struct icmphdr)]; 
-}; 
-
-// Calculating the Check Sum 
-unsigned short checksum(void *b, int len) 
-{ unsigned short *buf = b; 
-	unsigned int sum=0; 
-	unsigned short result; 
-
-	for ( sum = 0; len > 1; len -= 2 ) 
-		sum += *buf++; 
-	if ( len == 1 ) 
-		sum += *(unsigned char*)buf; 
-	sum = (sum >> 16) + (sum & 0xFFFF); 
-	sum += (sum >> 16); 
-	result = ~sum; 
-	return result; 
-} 
-
 
 // Interrupt handler 
 void intHandler(int dummy) 
